4cd52d455ccce3692ad6326b1f6fd9938d8052c3
Kwanghyun On <kwanghyun.on@gmail.com>
2020-04-25 16:20:40 +0900

    Add test() function in MetaLearner

--- models/metalearner.py
+++ models/metalearner.py
@@ -8,6 +8,7 @@ class MetaLearner:
     def __init__(self, main_net, meta_net, config, device=None):
         self.main_net = main_net
         self.meta_net = meta_net
+        self.acc_matrix = None
         self.config = config
         if device is None:
             self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -40,14 +41,43 @@ class MetaLearner:
                         cur_grads = self.main_net.compute_gradient(cur_inputs, cur_labels)
                         joint_grads = self.main_net.compute_gradient(joint_inputs, joint_labels)
                         cur_weights = self.main_net.get_model_weight()
-
                         meta_inputs = torch.cat((prev_grads, cur_grads, cur_weights), dim=0)
 
-                        # update main network with modified gradient
                         imp = self.meta_net.model(meta_inputs)
-                        cur_grads = self.config.alpha * imp * (cur_weights - prev_weights)
+                        cur_grads += self.config.alpha * imp * (cur_weights - prev_weights)
                         self.main_net.apply_gradient(cur_grads)
                         self.main_net.optimizer.step()
-
-                        # update meta network
                         self.meta_net.train_single_batch(meta_inputs, joint_grads)
+
+    def test(self, data_sequence):
+        n = len(data_sequence)
+        self.acc_matrix = torch.zeros(n, n)
+        for i in range(1, n):
+            prev_data_loader = DataLoader(dataset=data_sequence[i-1], batch_size=self.config.batch_size,
+                                          shuffle=True)
+            if i == 1:
+                self.main_net.train(prev_data_loader)
+                self.acc_matrix[0][0] = self.main_net.test(prev_data_loader)
+
+            prev_grads = self.main_net.abs_sum_of_gradient(prev_data_loader)
+            prev_weights = self.main_net.get_model_weight()
+            cur_data_loader = DataLoader(dataset=data_sequence[i], batch_size=self.config.batch_size,
+                                         shuffle=True)
+
+            for main_epoch in range(self.config.num_epochs_per_task):
+                for cur_inputs, cur_labels in cur_data_loader:
+                    for v in [cur_inputs, cur_labels]:
+                        v = v.to(self.device)
+                    cur_grads = self.main_net.compute_gradient(cur_inputs, cur_labels)
+                    cur_weights = self.main_net.get_model_weight()
+                    meta_inputs = torch.cat((prev_grads, cur_grads, cur_weights), dim=0)
+
+                    imp = self.meta_net.model(meta_inputs)
+                    cur_grads += self.config.alpha * imp * (cur_weights - prev_weights)
+                    self.main_net.apply_gradient(cur_grads)
+                    self.main_net.optimizer.step()
+
+            for j in range(i+1):
+                self.acc_matrix[i][j] = self.main_net.test(DataLoader(dataset=data_sequence[j],
+                                                                      batch_size=self.config.batch_size,
+                                                                      shuffle=True))
