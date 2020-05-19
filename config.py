9beb9de1f027b04894b4cc9cdfd53d2df820af9a
KwanghyunOn <kwanghyun.on@gmail.com>
2020-05-19 09:48:24 +0000

    Add EWCLearnerConfig in config.py

--- config.py
+++ config.py
@@ -1,13 +1,18 @@
 
 class Config:
     data_dir = "datasets/"
-    log_dir = "logs/exp1/base/"
-    result_path = "results/exp1/base.txt"
+    log_dir = "logs/exp1/EWC/"
+    result_path = "results/exp1/EWC.txt"
 
 class BaseLearnerConfig(Config):
     seq_len = 10
     batch_size = 256
-    num_epochs_main = 1
+    num_epochs_per_task = 1
+
+class EWCLearnerConfig(Config):
+    seq_len = 10
+    batch_size = 256
+    alpha = 0.1
     num_epochs_per_task = 1
 
 class MetaLearnerConfig(Config):
